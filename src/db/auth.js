81fba4c3ebf7d7a94acd31736b5aa60fb95f5cf7
Harold Bell <hbell@m-hbell.neb.com>
2021-11-22 13:23:28 -0500

    first commit

---
+++ src/db/auth.js
@@ -0,0 +1,6 @@
+const authOptions = {
+  user: 'u$hbell_qb',
+  key: 'peaPc0x8uLmQxw7K'
+}
+
+export {authOptions};
---
+++ src/tools/serverFunctions.js
@@ -0,0 +1,23 @@
+import {authOptions} from '../db/auth.js'
+console.log(authOptions)
+// query and return based on date / citations
+export async function getEntries (apiUrl) {
+  const myHeaders = new Headers();
+  const baseEncodedAuth = btoa(`${authOptions.user}:`);
+  console.log(baseEncodedAuth);
+  myHeaders.append('Authorization', `Basic ${baseEncodedAuth}`);
+  // myHeaders.append('Accept', `application/json`);
+
+  let entries = await fetch(apiUrl, {
+    method: 'GET',
+    headers: myHeaders
+  })
+    .then(response => response.json())
+    .then((response) => {
+      console.log(response.error.message)
+      return response;
+    })
+    .catch(err => console.log(err))
+
+  return entries;
+}

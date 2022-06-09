7ff712faa4ec1a69c36beb3faf6f2d0eb5d17720
Harold Bell <hbell@m-hbell.neb.com>
2022-06-09 15:10:32 -0400

    have barchart added with sample data ... format real data for the chart next

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@ import logo from './logo.png';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { Spinner, Button } from 'reactstrap';
+import { Button } from 'reactstrap';
+
+import BarChartDisplay from './Components/BarChartDisplay.js';
+
 
 const instruments = ['All Instruments', 'MiSeq', 'NextSeq', 'NovaSeq']
+
+// NOTES //
+// make a bar chart with # of runs per instrument
+// make scatter plot of CD, per instrument/all - https://recharts.org/en-US/examples/ThreeDimScatterChart
+// make scatter plot of EY, per instrument/all - https://recharts.org/en-US/examples/ThreeDimScatterChart
+// make scatter plot of CPF, per instrument/all - https://recharts.org/en-US/examples/ThreeDimScatterChart'
+// make a plot of CD vs CPF 
+
 
 class App extends React.Component {
 
@@ -14,7 +25,8 @@ class App extends React.Component {
 
     this.state = {
       records: null,
-      stats: null,
+      data: null,
+      dataToPlot: null,
     }
 
     this.showData = this.showData.bind(this);
@@ -46,27 +58,34 @@ class App extends React.Component {
     let allData = this.mapFileInfo(allFiles)
     let miseqData = this.mapFileInfo(miseqFiles)
     let nextseqData = this.mapFileInfo(nextseqFiles)
-    let novaseqData = this.mapFileInfo(novaseqFiles)
+    let novaseqData = this.mapFileInfo(novaseqFiles, 'Novaseq')
 
-    // set to state 
+    // set to state with keys matching instruments 
     this.setState({
       records: allFiles,
-      allData,
-      miseqData,
-      nextseqData,
-      novaseqData
+      data: {
+        'All Instruments': allData,
+        'MiSeq': miseqData,
+        'NextSeq': nextseqData,
+        'NovaSeq': novaseqData
+      }
     }, () => {
-      console.log('data separated by instrument')
+      // console.log('data separated by instrument')
     });
 
   }
 
-  // this funcion will read the desired info from the html strings and create and basic object with the info
+  // this function will read the desired info from the html strings and create an object with the info
   // ex {CD: ..., CPF: ..., EY: ..., Q30: ...} for each run
-  mapFileInfo(htmlStrings) {
+  mapFileInfo(htmlStrings, instrument) {
 
     let totalRunsInfo = [];
-    let statsNeeded = ['Date', 'Run Type', 'Cluster Density', 'Clusters Passing Filter', 'Estimated Yield', 'Q30', 'Sample @'];
+    let statsNeeded = ['Submittor Name', 'Date', 'Run Type', 'Cluster Density', 'Clusters Passing Filter', 'Estimated Yield', 'Q30', 'Sample @'];
+
+    // Nova seq's have a 'Library Concentration' entry instead of 'Sample @'
+    if (instrument === 'Novaseq') {
+      statsNeeded[statsNeeded.indexOf('Sample @')] = 'Library Concentration'
+    }
 
     for (let i = 0; i < htmlStrings.length; i++) {
       let file = htmlStrings[i].default;
@@ -75,69 +94,48 @@ class App extends React.Component {
       // *try to find a fix for the date, it has too much info*
       statsNeeded.forEach((stat, i) => {
         let ind = file.indexOf(stat);
-        let info = file.slice(ind, ind + 35);
-        info = info.slice(info.indexOf(':') + 1, info.indexOf('<')).trim();
+        let info = file.slice(ind, ind + 50);
+
+        if (stat === 'Sample @') {
+          info = info.slice(0, info.indexOf('nM') + 2).trim();
+        } else {
+          info = info.slice(info.indexOf(':') + 1, info.indexOf('<')).trim();
+        }
+
+        if (info.indexOf('\n') > -1) {
+          info = info.slice(0, info.indexOf('\n'));
+        }
+
         infoObj[stat] = info;
       });
       totalRunsInfo.push(infoObj);
-    }
 
+    }
+    // console.log(totalRunsInfo[0])
     return totalRunsInfo;
 
   }
 
   // this will extract one metric from the data set, ex Cluster Density, and create an array for recharts
   pullFieldData(field, data) {
+    // {
+    //   name: 'Page A',
+    //   uv: 4000,
+    //   pv: 2400,
+    //   amt: 2400,
+    // }
 
-  }
+    console.log(field)
+    console.log(data.length)
 
-  renderSpinners() {
-    return (
-      // hide when data is there
-      <div className="spinners-holder">
-        <Spinner
-          className="spinner"
-          color="primary"
-          type="grow"
-        >
-        </Spinner>
-        <Spinner
-          className="spinner"
-          color="primary"
-          type="grow"
-        >
-        </Spinner>
-        <Spinner
-          className="spinner"
-          color="primary"
-          type="grow"
-        >
-        </Spinner>
-      </div>
-    )
-  }
 
-  renderEntries() {
-    console.log('rendering entries');
-  }
-
-  renderStats() {
-    console.log('rendering stats');
-
-    return (
-      <div>
-        {this.state.entries.map((entry) => {
-          console.log(entry)
-          return (
-            <div>Hello!</div>
-          )
-        })}
-      </div>
-    )
   }
 
   showData(instrument) {
     console.log(`showing data for ${instrument}`)
+
+    let data = this.state.data[instrument];
+    console.log(data)
   }
 
   render() {
@@ -160,21 +158,15 @@ class App extends React.Component {
 
         <div className="App-content">
 
-          {!this.state.records ?
-            <div className="loading-spinners">
-              <p>just a moment ... </p>
-
-              {this.renderSpinners()}
-
-            </div>
-            : null}
-
           <div className="data-buttons">
             {instruments.map((instrument) => {
               return <Button key={instrument} color="primary btn-lg" onClick={() => this.showData(instrument)}>{instrument}</Button>
             })}
           </div>
 
+          {this.state.data ?
+            <BarChartDisplay data={this.state.dataToPlot} />
+            : null}
         </div>
       </div>
     );
---
+++ src/Components/BarChartDisplay.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import '../App.css';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+]
+class BarChartDisplay extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    console.log('Scatter Plot Display mounted')
+    console.log(this.props.data)
+  }
+
+  render() {
+    return (
+      <div className="scatter-plot-display">
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart
+            width={'80%'}
+            height={'50%'}
+            data={data}
+            margin={{
+              top: 20
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="pv" stackId="a" fill="#8884d8" />
+            <Bar dataKey="uv" stackId="a" fill="#82ca9d" />
+            <Bar dataKey="amt" stackId="a" fill="#82ca9d" />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+    );
+  }
+}
+
+export default BarChartDisplay;
